235659d6a28ba4a4342349bdccad181468d0ac79
Mathieu <mathieu.naud@hotmail.com>
2015-10-09 20:37:51 -0300

    -> Camera monte -> Objets détruits -> Objets instanciés

---
+++ UnityMilfA2015/Assets/Scripts/CameraAndCollider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAndCollider : MonoBehaviour
+{
+    private Camera Cam;
+    
+    public float CameraSpeed;
+    public GameObject Prefab;
+    public int HauteurMax;
+
+    void Awake()
+    {
+        Cam = GetComponentInParent<Camera>();
+        CameraSpeed = 3.3f;
+    }
+
+    void Update()
+    {
+        if(Cam.transform.position.y < HauteurMax)
+            Cam.transform.Translate(Vector3.up * CameraSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+        Instantiate(Prefab).transform.Translate(Random.Range(-7, 7), Cam.transform.position.y + 8, 0);
+    }
+}
