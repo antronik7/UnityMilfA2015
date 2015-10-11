53f2c85f74c41ea45efd058ec263b1bad278ee42
antronik7 <antoineberthiaumedutil@gmail.com>
2015-10-10 22:59:01 -0400

    Le prefab de player

--- UnityMilfA2015/Assets/Scripts/NbrDeAmmo.cs
+++ UnityMilfA2015/Assets/Scripts/NbrDeAmmo.cs
@@ -13,15 +13,20 @@ public class NbrDeAmmo : MonoBehaviour {
     
     void OnGUI()
     {
+        munition = this.GetComponent<PlayerShoot>().munition;
         //On doit verifer dans quel sens le joueur va
-        if(this.GetComponent<PlayerController>().facingRight)
+        if (this.GetComponent<PlayerController>().facingRight)
         {
-
+            multiplicateurSelonLaDirection = 1;
+        }
+        else
+        {
+            multiplicateurSelonLaDirection = -1;
         }
 
         Vector3 getPixelPos = Camera.main.WorldToScreenPoint(gameObject.transform.position);
         getPixelPos.y = Screen.height - getPixelPos.y;
-        GUI.Label(new Rect((getPixelPos.x - (modifierPositionXTest * gameObject.transform.localScale.x)), getPixelPos.y - (modifierPositionYTest * gameObject.transform.localScale.y), 100f, 100f), munition.ToString(), guiStylePoliceNbrAmmo);
+        GUI.Label(new Rect((getPixelPos.x - (modifierPositionXTest * multiplicateurSelonLaDirection * gameObject.transform.localScale.x)), getPixelPos.y - (modifierPositionYTest * gameObject.transform.localScale.y), 100f, 100f), munition.ToString(), guiStylePoliceNbrAmmo);
     }
 
 
