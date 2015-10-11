9304738dd1d465082b5b9c0ffda88711faf08869
antronik7 <antoineberthiaumedutil@gmail.com>
2015-10-11 08:27:05 -0400

    Son PickUP

--- UnityMilfA2015/Assets/Scripts/RamasserPixels.cs
+++ UnityMilfA2015/Assets/Scripts/RamasserPixels.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class RamasserPixels : MonoBehaviour 
 {
     private PlayerShoot Script;
-    public GameObject[] hurtFeedBack;
+    public GameObject[] pickUPFeedBack;
+    public AudioClip pickUpSound;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -13,11 +14,26 @@ public class RamasserPixels : MonoBehaviour
             Script = other.GetComponent<PlayerShoot>();
 
             if (this.gameObject.name == "Munition1(Clone)")
+            {
+                AudioSource.PlayClipAtPoint(pickUpSound, transform.position, 10f);
+                Instantiate(pickUPFeedBack[0], transform.position, Quaternion.identity);
                 Script.munition++;
+            }
+            Script.munition++;
             if (this.gameObject.name == "Munition5(Clone)")
+            {
+                AudioSource.PlayClipAtPoint(pickUpSound, transform.position, 10f);
+                Instantiate(pickUPFeedBack[1], transform.position, Quaternion.identity);
                 Script.munition += 5;
+            }
+                
             if (this.gameObject.name == "Munition10(Clone)")
+            {
+                AudioSource.PlayClipAtPoint(pickUpSound, transform.position, 10f);
+                Instantiate(pickUPFeedBack[2], transform.position, Quaternion.identity);
                 Script.munition += 10;
+            }
+                
 
             Destroy(this.gameObject);
         }
